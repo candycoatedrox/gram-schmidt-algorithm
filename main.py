a7b214b0573bebb0b8e9c3c32708f980dc90bc8c
candycoatedrox <60051903+candycoatedrox@users.noreply.github.com>
2025-05-15 13:17:18 -0400

    Added framework for the algorithm

---
+++ main.py
@@ -0,0 +1,130 @@
+# pg 96 of notes
+import sys
+import time
+import copy
+
+# make the whole thing function on fractions???
+# currently started to make it function on floats but i can fix that
+
+def slowType(text):
+    """Makes the program more user-friendly by appearing to type out the input."""
+    for l in text:
+        print(l, end="")
+        sys.stdout.flush()
+        time.sleep(.025)
+
+def slowInput(prompt):
+    # how do i do this???
+    pass
+
+def identity(dimV):
+    # each list is a row
+    matrix = []
+    for r in range(dimV):
+        row = []
+        for c in range(dimV):
+            if r == c:
+                row.append(1)
+            else:
+                row.append(0)
+        matrix.append(copy.deepcopy(row))
+
+    return matrix
+
+def normalize(v):
+    pass
+
+def getDim():
+    valid = False
+    while not(valid):
+        compNum = input("Enter the number of vector components: ")
+        try:
+            compNum = int(compNum)
+            valid = True
+        except:
+            print("Please enter an integer.")
+
+    valid = False
+    while not(valid):
+        dimV = input("Enter the number of basis vectors: ")
+        try:
+            dimV = int(dimV)
+            if dimV > compNum:
+                print("Dimension must be less than or equal to the number of components.")
+            else:
+                valid = True
+        except:
+            print("Please enter an integer.")
+    
+    return (compNum, dimV)
+
+def getInnerProd(dimV):
+    standard = input("Standard inner product? (y/n): ")
+    if standard == "y" or standard == "Y":
+        return identity(dimV)
+    
+    Gij = []
+    for r in range(dimV):
+        valid = False
+        while not(valid):
+            rawRow = input(f"Enter row {r + 1} of Gij, with components separated by spaces: ")
+            rStr = rawRow.split()
+
+            row = []
+            if len(rStr) == dimV:
+                valid = True
+                for x in rStr:
+                    try:
+                        xNum = float(x)
+                        row.append(xNum)
+                    except:
+                        valid = False
+                        print("Please enter a list of integers or floats.")
+            else:
+                print(f"Please enter a row with {dimV} components.")
+        
+        Gij.append(copy.deepcopy(row))
+    
+    # should hypothetically have another check here that it's symmetrical...
+    # or just. change it so you just enter, ex. G11, G12, G22 for dim = 2
+    return Gij
+
+def getBasis(compNum, dimV):
+    print("\n")
+    basis = []
+    for n in range(dimV):
+        valid = False
+        while not(valid):
+            rawVector = input(f"Enter basis vector {n + 1}, with components separated by spaces: ")
+            vStr = rawVector.split()
+
+            vector = []
+            if len(vStr) == compNum:
+                valid = True
+                for x in vStr:
+                    try:
+                        xNum = float(x)
+                        vector.append(xNum)
+                    except:
+                        valid = False
+                        print("Please enter a list of integers or floats.")
+            else:
+                print(f"Please enter a vector with {compNum} components.")
+        
+        basis.append(copy.deepcopy(vector))
+
+    return basis
+
+def gramSchmidt(basis, Gij):
+    pass
+
+def main():
+    compNum, dimV = getDim()
+    Gij = getInnerProd(compNum)
+    basis = getBasis(compNum, dimV)
+    print(basis, Gij)
+
+    orthonormal = gramSchmidt(basis)
+
+if __name__ == '__main__':
+    main()
